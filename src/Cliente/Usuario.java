f8c382eb5339b8d2e9602a27ad7ea75637437e40
Yello-Platipus <danieltinez1@gmail.com>
2023-04-18 12:05:12 +0200

    aaaaaaaaaaaaaa

--- src/Cliente/Usuario.java
+++ src/Cliente/Usuario.java
@@ -34,31 +34,17 @@ public class Usuario {
     }
 
 
+    // Getters
     public String getId() {
         return id;
     }
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public InetAddress getIp() {
         return ip;
     }
-    public void setIp(InetAddress ip) {
-        this.ip = ip;
-    }
-
     public int getPuerto() {
         return puerto;
     }
-    public void setPuerto(int puerto) {
-        this.puerto = puerto;
-    }
-
     public Set<String> getInfo() {
         return info;
-    }
-    public void setInfo(Set<String> info) {
-        this.info = info;
     }
 }
--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -8,21 +8,23 @@ import java.util.*;
 
 public class Servidor {
     private ServerSocket serverSocket;
-    private InetAddress ip;
     private static final int puerto = 8080;
     private Set<Usuario> conectados;
     private Map<String, Usuario> quienTiene;
     private ArrayList<Thread> hilos;
 
-    public Servidor(InetAddress ip) {
+    public Servidor() {
         try {
             this.serverSocket = new ServerSocket(puerto);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        this.ip = ip;
         conectados = new HashSet<>();
         hilos = new ArrayList<>();
         quienTiene = new HashMap<>();
+    }
+
+    public static int getPuerto() {
+        return puerto;
     }
 }
