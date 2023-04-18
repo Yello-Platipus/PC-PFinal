7e287322719b446206e7d066e1d65686cf0dd10a
Yello-Platipus <danieltinez1@gmail.com>
2023-04-18 18:47:39 +0200

    uvxildosaispa

--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -14,7 +14,7 @@ public class Servidor {
     private ServerSocket serverSocket;
     private static final int puerto = 8080;
     private Set<Usuario> conectados;
-    private Map<String, Usuario> quienTiene;
+    private Map<String, ArrayList<Usuario>> quienTiene;
 
     public Servidor() throws IOException {
         try {
@@ -27,10 +27,6 @@ public class Servidor {
         start(puerto);
     }
 
-    public static int getPuerto() {
-        return puerto;
-    }
-
     private void start(int puerto) throws IOException {
         Socket cliente;
         while(true){
@@ -38,6 +34,35 @@ public class Servidor {
             new OyenteCliente(cliente, this).start();
 
         }
+    }
+
+    public synchronized void anadirUsuario(Usuario usuario){
+        conectados.add(usuario);
+        for(String archivo : usuario.getInfo())
+            quienTiene.get(archivo).add(usuario);
+    }
+
+    public synchronized void eliminarUsuario(Usuario usuario){
+        conectados.remove(usuario);
+        for(String archivo : usuario.getInfo())
+            quienTiene.get(archivo).remove(usuario);
+    }
+
+    public synchronized ArrayList<String> getInfo(){
+        return new ArrayList<>(quienTiene.keySet());
+    }
+
+    public synchronized Usuario getUsuario(String info){
+        // TODO puede que haya que devolver un Socket/ServerSocket en vez de un Usuario
+        ArrayList<Usuario> usuarios = quienTiene.get(info);
+        if(usuarios.size() == 0)
+            return null;
+        else
+            return usuarios.get(0);
+    }
+
+    public static int getPuerto() {
+        return puerto;
     }
 
 }
